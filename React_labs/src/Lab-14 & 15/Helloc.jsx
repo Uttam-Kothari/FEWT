b693fbbc8e2b947a240920aa807145bcbcbad71d
Uttam Kothari <uttamkothari6934@gmail.com>
2026-09-22 05:49:45 +0530

    Add files via upload

---
+++ React_labs/src/Lab-14 & 15/Helloc.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class  Helloc extends Component{
+   render(){
+    return (<>
+        <h1>Hello in class</h1>
+    </>
+    )
+   }
+}
+export default Helloc
---
+++ React_labs/src/Lab-14 & 15/Hellof.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function Hellof() {
+    return (
+        <>
+         <h1>Hello in function</h1>
+        </>
+    )
+   
+}
+
+export default Hellof
+
