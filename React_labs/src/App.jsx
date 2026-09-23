e868f137d963c6ebe513c06277649cc793be5065
Uttam Kothari <uttamkothari6934@gmail.com>
2026-09-23 20:52:32 +0530

    Update App.jsx

--- React_labs/src/App.jsx
+++ React_labs/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 
-import Hellof from './Lab-14/Hellof'
-import Helloc from './Lab-14/Helloc'
+// import Hellof from './Lab-14/Hellof'
+// import Helloc from './Lab-14/Helloc'
+
+// import  Navbar from './Lab-15extra/components/Navbar'
+// import Home from './Lab-15extra/components/Home'
+// import Contact from './Lab-15extra/components/Contact'
+// import Footer from './Lab-15extra/components/Footer'
+
+import A1C from './Lab-16/A1'
+import A2 from './Lab-16/A2'
+import A3 from './Lab-16/A3'
+
 
 
 function App() {
@@ -18,6 +25,27 @@ function App() {
 
             {/* <Helloc /> */}
             {/* <Hellof /> */}
+
+
+            {/* Lab-Extera */}
+
+            {/* <Home/> */}
+            {/* <Contact/> */}
+
+
+            {/* Lab-16 */}
+
+            {/* A1  */}
+             <A1C/> 
+
+            {/* A2 */}
+             <A2 /> 
+
+            {/* A3 */}
+             <A3/> 
+
+
+
         </>
     )
 }
